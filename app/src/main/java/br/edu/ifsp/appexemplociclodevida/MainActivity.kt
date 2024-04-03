fd75b9a03ac4ab84c352c29c575333922beb04e1
Ednilson Rossi <ednilsonrossi@gmail.com>
2024-04-02 21:12:59 -0300

    Correção de erro de digitação.

--- app/src/main/java/br/edu/ifsp/appexemplociclodevida/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/appexemplociclodevida/MainActivity.kt
@@ -2,17 +2,13 @@ package br.edu.ifsp.appexemplociclodevida
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.appexemplociclodevida.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "CICLO_DE_VIDA"
-    private var chages: Int = 0
+    private var changes: Int = 0
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,43 +17,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Log.v(TAG, "Executando onCreate()")
-        chages += 1
+        changes += 1
     }
 
     override fun onStart() {
         super.onStart()
 
         Log.v(TAG, "Executando onStart()")
-        chages += 1
+        changes += 1
     }
 
     override fun onResume() {
         super.onResume()
 
         Log.v(TAG, "Executando onResume()")
-        chages += 1
+        changes += 1
 
-        binding.textviewChanges.text = "Mudanças de estado: $chages vezes."
+        binding.textviewChanges.text = "Mudanças de estado: $changes vezes."
     }
 
     override fun onPause() {
         super.onPause()
 
         Log.v(TAG, "Executando onPause()")
-        chages += 1
+        changes += 1
     }
 
     override fun onStop() {
         super.onStop()
 
         Log.v(TAG, "Executando onStop()")
-        chages += 1
+        changes += 1
     }
 
     override fun onDestroy() {
         Log.v(TAG, "Executando onDestroy()")
 
-        Log.v(TAG, "Esse é meu fim, foram $chages mudanças de estados durante meu ciclo de vida.")
+        Log.v(TAG, "Esse é meu fim, foram $changes mudanças de estados durante meu ciclo de vida.")
         super.onDestroy()
     }
 }
